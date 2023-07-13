03a1bf307f0c04c8185af30ecbb769c6259aa56c
fath-nasrudin <fath.nasrudin.i@gmail.com>
2023-07-13 18:21:20 +0700

    Add grid

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
 // Sync year in the footer
 document.querySelector('#yearFooter').textContent = new Date().getFullYear();
+
+const LENGTH = 16;
+
+const createCell = () => {
+  const cell = document.createElement('div');
+  cell.classList.add('cell');
+  return cell;
+};
+
+const createRowOfCells = (totalCell) => {
+  const row = document.createElement('div');
+  row.classList.add('row');
+  for (let j = 0; j < totalCell; j++) {
+    row.append(createCell());
+  }
+  return row;
+};
+
+const generateBoard = (totalLength = 64) => {
+  const board = document.querySelector('#board');
+  for (let i = 0; i < totalLength; i++) {
+    const row = createRowOfCells(totalLength);
+    board.append(row);
+  }
+};
+
+generateBoard(LENGTH);
