6028626a10ed4ae5bad737ee5a9cdc03d30b6153
fath-nasrudin <fath.nasrudin.i@gmail.com>
2023-07-13 23:57:16 +0700

    ref: Refactor for readability

--- main.js
+++ main.js
@@ -1,35 +1,16 @@
 // Sync year in the footer
 document.querySelector('#yearFooter').textContent = new Date().getFullYear();
 
-const LENGTH = 16;
-let mouseHoverState = false;
-let mouseDownState = false;
-const getMouseHoverState = () => mouseHoverState;
-const getMouseDownState = () => mouseDownState;
-
+// we use mousedown alot. so we disable the drag default function;
 document.addEventListener('dragstart', (e) => {
   e.preventDefault();
 });
 
-document.addEventListener('mousedown', function (e) {
-  mouseDownState = true;
-});
-
-document.addEventListener('mouseup', function (e) {
-  mouseDownState = false;
-});
+const LENGTH = 16;
 
 const createCell = () => {
   const cell = document.createElement('div');
   cell.classList.add('cell');
-
-  cell.addEventListener('mousedown', (e) =>
-    e.currentTarget.classList.add('filled')
-  );
-
-  cell.addEventListener('mouseover', function () {
-    if (getMouseDownState()) this.classList.add('filled');
-  });
   return cell;
 };
 
@@ -42,6 +23,32 @@ const createRowOfCells = (totalCell) => {
   return row;
 };
 
+// pen eraser ability
+let mouseHoverState = false;
+let mouseDownState = false;
+const getMouseHoverState = () => mouseHoverState;
+const getMouseDownState = () => mouseDownState;
+
+// detect mouse up and down and hover in out
+const board = document.querySelector('#board');
+board.addEventListener('mouseover', () => (mouseHoverState = true));
+board.addEventListener('mouseout', () => (mouseHoverState = false));
+document.addEventListener('mousedown', () => (mouseDownState = true));
+document.addEventListener('mouseup', () => (mouseDownState = false));
+
+const populateCellsListeners = () => {
+  const cells = document.querySelectorAll('.cell');
+  Array.from(cells).forEach((cell) => {
+    cell.addEventListener('mousedown', (e) =>
+      e.currentTarget.classList.add('filled')
+    );
+
+    cell.addEventListener('mouseover', function () {
+      if (getMouseDownState()) this.classList.add('filled');
+    });
+  });
+};
+
 const generateBoard = (totalLength = 64) => {
   const board = document.querySelector('#board');
   board.replaceChildren(); // reset
@@ -51,6 +58,8 @@ const generateBoard = (totalLength = 64) => {
     const row = createRowOfCells(totalLength);
     board.append(row);
   }
+
+  populateCellsListeners();
 };
 
 generateBoard(LENGTH);
